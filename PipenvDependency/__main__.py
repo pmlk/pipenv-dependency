1e9491bb63b6dd19c01cf83caf559192f2c21ce0
pmlk <pmlk@trainofcode.com>
2018-01-20 23:27:46 +0100

    initial commit

---
+++ PipenvDependency/__main__.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+if __name__ == "__main__":
+    np_array = np.array([[1, 2], [11, 22]])
+    print(np_array)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PipenvDependency',
+    version='0.0.0',
+    packages=['PipenvDependency'],
+    include_package_data=True,
+    install_requires=['numpy'],
+)
